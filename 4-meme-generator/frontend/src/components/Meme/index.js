e180d3139c7d800ca7ea24663520b9e2150fc807
Burakcan Aygün <34108859+burakcanaygun@users.noreply.github.com>
2021-11-11 16:36:30 +0300

    Handle meme state if it's empty

--- 4-meme-generator/frontend/src/components/Meme/index.js
+++ 4-meme-generator/frontend/src/components/Meme/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import {Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import { useSelector } from "react-redux";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import TextCaption from "../TextCaption";
 import Loading from "../Loading";
+import { useNavigate } from 'react-router';
 
 const Meme = () => {
     const meme = useSelector(state => state.memes.meme);
     const loading = useSelector(state => state.memes.loading);
+    const navigation = useNavigate();
     return (
         <>
-            {loading ? (<Loading/>) : (
+            {loading ? (<Loading />) : meme.id ? (
                 <Grid mt={3} display={"flex"} justifyContent={"center"} alignItems={"center"}
-                      textAlign={"center"} flexDirection={"column"}>
+                    textAlign={"center"} flexDirection={"column"}>
                     <Card key={meme.id}
-                          style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <Typography variant={"h4"}>
                             {meme.name}
                         </Typography>
                         <CardMedia component={"img"} image={meme.url} alt={meme.name} height={'auto'}
-                                   style={{width: "50%"}}/>
+                            style={{ width: "50%" }} />
                         <CardContent>
-                            <TextCaption/>
+                            <TextCaption />
                         </CardContent>
                     </Card>
+                </Grid>
+            ) : (
+                <Grid align={"center"}>
+                    <Typography variant={"h4"} align={"center"}> There is no meme for you sorry. </Typography>
+                    <Button variant={"contained"} color={"primary"} onClick={() => navigation('/')}> Go Back Home </Button>
                 </Grid>
             )}
         </>
